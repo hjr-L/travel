c8a462ad52d909be733fa793eb8fb8caaaf57a91
hjr <1151517639@qq.com>
2019-12-22 13:36:33 +0800

    小图标区域完成

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -1,30 +1,85 @@
 <template>
     <div class="icons">
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-
-        </div>
+        <swiper class="swiper-contaier" >
+                <swiper-slide v-for="(page, index) of pages" :key="index" >
+                  <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                      <img class="icon-img-content" :src="item.imgUrl">
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                  </div>
+                </swiper-slide>
+        </swiper>
     </div>
 </template>
 <script>
 export default {
-  name: 'HomeIcons'
-
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/hotel.png',
+        desc: '酒店'
+      }, {
+        id: '0002',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/flight.png',
+        desc: '机票'
+      }, {
+        id: '0003',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/package.png',
+        desc: '度假'
+      }, {
+        id: '0004',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/train.png',
+        desc: '火车票'
+      }, {
+        id: '0005',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/piao.png',
+        desc: '景点门票'
+      }, {
+        id: '0006',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png',
+        desc: '自然风光'
+      }, {
+        id: '0007',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/48/cb72b99b71974c02.png',
+        desc: '生活休闲'
+      }, {
+        id: '0008',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/f5/a963333e1fa802.png',
+        desc: '本地玩乐'
+      }, {
+        id: '0009',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: '全部玩乐'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
     /* @import url('../assets/style/varibles.styl'); */
-    @import "../assets/style/varibles.styl"
-    .icons{
-        overflow: hidden;
+    @import "../assets/style/varibles.styl";
+    @import "../assets/style/mixins.styl";
+    .icons >>> .swiper-contaier{
         width: 100%;
         height: 0;
         padding-bottom: 50%;
         /* 宽高比50% */
-        background: rgb(221, 33, 33);
     }
     .icon{
         position: relative;
@@ -33,7 +88,6 @@ export default {
         width: 25%;
         height: 0;
         padding-bottom: 25%;
-        background: #989898;
     }
     .icon-img{
         position: absolute;
@@ -43,7 +97,6 @@ export default {
         bottom: .44rem;
         box-sizing: border-box;
         padding: .1rem;
-        background: blue;
     }
     .icon-img-content{
         height: 100%;
@@ -59,5 +112,6 @@ export default {
         line-height: .44rem;
         color: $darkTextColor;
         text-align: center;
+        ellipsis();
     }
 </style>
