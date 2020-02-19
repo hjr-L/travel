34b50fba87c89ac1f6816ef8ba177c07be58ab67
hjr <1151517639@qq.com>
2020-02-19 10:51:07 +0800

    旅游详情页

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,17 +2,42 @@
     <div>
        <DetailBanner></DetailBanner>
        <DetailHeader></DetailHeader>
-       <div class="content"></div>
+       <div class="content">
+         <DetailList  :list="list"></DetailList>
+       </div>
     </div>
 </template>
 <script>
 import DetailBanner from '../components/detail/Banner'
 import DetailHeader from '../components/detail/Header'
+import DetailList from '../components/detail/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票--信阳旅游局售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '儿童票'
+      }, {
+        title: '学生票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
